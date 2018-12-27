64c1985832452a7087329a9ed37e90389e67b3c9
vantichelen <vantichelen@192.168.1.108>
2018-12-27 14:28:57 +0100

    getters en setters toegevoegd

--- src/javafxnewgittest/NewGitTest.java
+++ src/javafxnewgittest/NewGitTest.java
@@ -15,6 +15,20 @@ public class NewGitTest {
     public NewGitTest(String naam) {
         this.naam = naam;
     }
+
+    /**
+     * @return the naam
+     */
+    public String getNaam() {
+        return naam;
+    }
+
+    /**
+     * @param naam the naam to set
+     */
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
     
 }
 
